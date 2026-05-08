3bf029eb31d0acfbc28dcea988d068d9115620de
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-05-07 21:53:53 -0600

    Agregando ejercicio del reto 1071. Greatest Common Divisor of Strings

---
+++ array_string/greatestCommonDivisorStrings.java
@@ -0,0 +1,19 @@
+package array_string;
+
+public class greatestCommonDivisorStrings {
+    public String gcdOfStrings(String str1, String str2){
+        if(!(str1 +  str2).equals(str2 + str1)){
+            return "";
+        }
+        int limite = mcd(str1.length(), str2.length());
+
+        return str1.substring(0, limite);
+    }
+    private int mcd(int a, int b){
+        if(b == 0){
+            return a;
+        }
+
+        return mcd(b, a % b);
+    }
+}
