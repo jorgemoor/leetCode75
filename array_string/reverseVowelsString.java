1bce0cc8be093f4156b906833059d80ef8a84806
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-05-11 21:03:59 -0600

    Agregando ejercicio Array/String 345. Reverse Vowels String

---
+++ array_string/reverseVowelsString.java
@@ -0,0 +1,29 @@
+package array_string;
+
+public class reverseVowelsString {
+    public static String rvs(String s){
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+            while(left < right && !isVowel(arr[left])){
+                left++;
+            }
+            while(left < right && !isVowel(arr[right])){
+                right--;
+            }
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+
+        }
+        return new String(arr);
+    }
+    private static boolean isVowel(char ch){
+        ch = Character.toLowerCase(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'  || ch == 'u';
+    }
+}
--- main.java
+++ main.java
@@ -3,8 +3,9 @@ public class main {
     public static void main(String[] args) {
         int[] candies = {1,0,0,0,1};
         int extraCandies = 2;
+        String s = "IceCreAm";
 
-        System.out.println(canPlaceFlowers.cpf(candies, extraCandies));
+        System.out.println(reverseVowelsString.rvs(s));
 
 
     }
