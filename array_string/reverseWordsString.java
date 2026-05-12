82706301e7e821f04ae8440dd1b7a889838eff7f
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-05-12 11:26:23 -0600

    Agregando ejercicio Array/String 151. Reverse Words  String

---
+++ array_string/reverseWordsString.java
@@ -0,0 +1,21 @@
+package array_string;
+
+public class reverseWordsString {
+    public static String rws(String s) {
+        int j = s.length() - 1;
+        int b = 0;
+        StringBuilder a = new StringBuilder();
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == ' ' && i<j){
+                a.append(s.substring(i + 1, j + 1));
+                a.append(' ');
+                while (i >= 0 && s.charAt(i) == ' '){
+                    i--;
+                }
+                j = i;
+            }
+        }
+        a.append(s.substring(0, j + 1));
+        return a.toString().trim();
+    }
+}
--- main.java
+++ main.java
@@ -3,9 +3,9 @@ public class main {
     public static void main(String[] args) {
         int[] candies = {1,0,0,0,1};
         int extraCandies = 2;
-        String s = "IceCreAm";
+        String s = "the sky is blue";
 
-        System.out.println(reverseVowelsString.rvs(s));
+        System.out.println(reverseWordsString.rws(s));
 
 
     }
