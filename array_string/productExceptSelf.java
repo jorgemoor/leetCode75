5c5356eb2d00efacf1e66bdad1acbb693fb653e7
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-05-13 21:02:27 -0600

    Agregando ejercicio Array/String 238. Product of Array ExceptSelf

---
+++ array_string/productExceptSelf.java
@@ -0,0 +1,21 @@
+package array_string;
+
+public class productExceptSelf {
+    public static int[] pes(int[] nums) {
+        int n = nums.length;
+        int[] resultado = new int[n];
+
+        resultado[0] = 1;
+        for (int i = 1; i < n; i++) {
+            resultado[i] = resultado[i - 1] * nums[i - 1];
+
+        }
+        int right = 1;
+        for(int i = n - 1; i >= 0; i--){
+            resultado[i] *= right;
+            right *= nums[i];
+        }
+
+        return resultado;
+    }
+}
--- main.java
+++ main.java
@@ -1,11 +1,14 @@
 import array_string.*;
+
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
-        int[] candies = {1,0,0,0,1};
+        int[] candies = {1,2,3,4};
         int extraCandies = 2;
         String s = "the sky is blue";
 
-        System.out.println(reverseWordsString.rws(s));
+        System.out.println(Arrays.toString(productExceptSelf.pes(candies)));
 
 
     }
