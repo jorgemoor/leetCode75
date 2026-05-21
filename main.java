555a11418345e24ff8dd4a4a041bd28f21c521a4
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-05-21 11:06:01 -0600

    Completando ejercicio 1456. Max Bumber of Vowels in a Substring of Given Length

--- main.java
+++ main.java
@@ -5,12 +5,12 @@ public class main {
         char[] candies = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
         int[] x = {1,12,-5,-6,50,3};
         int extraCandies = 1;
-        String s = "abc";
+        String s = "aeiou";
         String t = "ahbgdc";
 
-        System.out.println(MaximumAverageSubarray.findMaxAverage(x, 4));
+        System.out.println(MaximumNumberVowelsSubstring.maxVowels(s, 3));
 
-        System.out.println((int)'b');
+
 
     }
 }
--- slidingwindow/MaximumNumberVowelsSubstring.java
+++ slidingwindow/MaximumNumberVowelsSubstring.java
@@ -2,5 +2,23 @@ package slidingwindow;
 
 public class MaximumNumberVowelsSubstring {
     public static int maxVowels(String s, int k){
+        int[] chars = new int[128];
+        chars['a']++;
+        chars['e']++;
+        chars['i']++;
+        chars['o']++;
+        chars['u']++;
+
+        char[] arr = s.toCharArray();
+        int current = 0;
+        for(int i = 0; i < k; i++) current += chars[arr[i]];
+
+        int max = current;
+        for(int i = 0, j = k; j < arr.length; i++, j++){
+            current -= chars[arr[i]];
+            current += chars[arr[j]];
+            max = Math.max(max, current);
+        }
+        return max;
     }
 }
