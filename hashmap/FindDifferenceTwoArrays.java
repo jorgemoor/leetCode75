4577927989fcc7a8c00ebe1bbe05971949857c63
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-06-09 20:11:38 -0600

    Actualizando el ejercicio HashMap 2215. Find the Difference of Two Arrays

--- hashmap/FindDifferenceTwoArrays.java
+++ hashmap/FindDifferenceTwoArrays.java
@@ -4,34 +4,33 @@ import java.util.*;
 
 public class FindDifferenceTwoArrays {
     public static List<List<Integer>> findDifferences(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        boolean[] n1 = new boolean[2001];
+        boolean[] n2 = new boolean[2001];
+
+        ArrayList<Integer> diff1 = new ArrayList<>();
+        ArrayList<Integer> diff2 = new ArrayList<>();
 
         for(int num : nums1){
-            set1.add(num);
+            n1[num + 1000] = true;
         }
+
         for(int num : nums2){
-            set2.add(num);
-        }
+            n2[num + 1000] = true;
 
-        List<Integer> diff1 = new ArrayList<>();
-        List<Integer> diff2 = new ArrayList<>();
-
-        for(int num : set1){
-            if(!set2.contains(num)){
-                diff1.add(num);
-            }
-        }
-        for(int num : set2){
-            if(!set1.contains(num)){
+            if(!n1[num + 1000]){
+                n1[num + 1000] = true;
                 diff2.add(num);
             }
         }
 
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(diff1);
-        result.add(diff2);
-        return result;
+        for(int num : nums1){
+            if(!n2[num + 1000]){
+                n2[num + 1000] = true;
+                diff1.add(num);
+            }
+        }
+
+        return List.of(diff1, diff2);
 
     }
 }
