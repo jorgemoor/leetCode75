10eb802193c7164365aa89473b599241ef7523a6
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-06-13 14:47:54 -0600

    Agregando ejercicio HashMap 1657. Determine if Two Strings Are Close

---
+++ hashmap/DetermineTwoStringsClose.java
@@ -0,0 +1,24 @@
+package hashmap;
+
+import java.util.Arrays;
+
+public class DetermineTwoStringsClose {
+    public static boolean closeStrings(String s1, String s2) {
+        if(s1.length() != s2.length()) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(char c : s1.toCharArray()) freq1[c - 'a']++;
+        for(char c : s2.toCharArray()) freq2[c - 'a']++;
+
+        for(int i = 0; i < 26; i++){
+            if((freq1[i] ==0) != (freq2[i] == 0)) return false;
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);
+    }
+}
--- main.java
+++ main.java
@@ -3,12 +3,13 @@ import hashmap.*;
 public class main {
     public static void main(String[] args) {
         char[] candies = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
-        int[] x = {1,6,};
+        int[] x = {1,6};
         int[] y = {2,4,6};
         int extraCandies = 1;
+        String word1 = "cabbba", word2 = "abbccc";
 
         long inicio = System.currentTimeMillis();
-        System.out.println(UniqueNumberOccurrences.isUnique(x));
+        System.out.println(DetermineTwoStringsClose.closeStrings(word1, word2));
         long fin = System.currentTimeMillis();
         System.out.println("Tiempo de Ejecucicon: " + (fin - inicio) + "ms");
 
