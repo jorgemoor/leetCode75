e645ec9f164f7ed264d1a2af4ef0897ae2770757
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-06-19 11:28:08 -0600

    Agregando ejercicio HashMap 2352. Equal Row and Column Pairs

--- hashmap/EqualRowColumnPairs.java
+++ hashmap/EqualRowColumnPairs.java
@@ -1,7 +1,26 @@
 package hashmap;
 
+import java.util.*;
+
+
 public class EqualRowColumnPairs {
     public static int equalPairs(int[][] grid){
+        int n = grid.length;
+        int col[] = new int[n];
+        int count = 0;
 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                col[j] = grid[j][i];
+
+            }
+            for(int k = 0; k < n; k++){
+                if(Arrays.equals(col, grid[k])){
+                    count++;
+                }
+            }
+
+        }
+        return count;
     }
 }
--- main.java
+++ main.java
@@ -5,11 +5,12 @@ public class main {
         char[] candies = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
         int[] x = {1,6};
         int[] y = {2,4,6};
+        int[][] grid = {{3,1,2,2}, {1,4,4,5}, {2,4,2,2}, {2,4,2,2}};
         int extraCandies = 1;
         String word1 = "cabbba", word2 = "abbccc";
 
         long inicio = System.currentTimeMillis();
-        System.out.println(DetermineTwoStringsClose.closeStrings(word1, word2));
+        System.out.println(EqualRowColumnPairs.equalPairs(grid));
         long fin = System.currentTimeMillis();
         System.out.println("Tiempo de Ejecucicon: " + (fin - inicio) + "ms");
 
