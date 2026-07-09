57e3d9bb90ecb7793de8619238c00ed4a065b170
Jorge Moo <jorgerobertomoo27@gmail.com>
2026-07-09 07:42:55 -0600

    Agregando ejercicio Stack 394. Decode String

--- main.java
+++ main.java
@@ -12,10 +12,10 @@ public class main {
         int[] y = {2,4,6};
         int[][] grid = {{3,1,2,2}, {1,4,4,5}, {2,4,2,2}, {2,4,2,2}};
         int extraCandies = 1;
-        String word1 = "erase*****", word2 = "abbccc";
+        String word1 = "abc3[cd]xyz", word2 = "abbccc";
 
         long inicio = System.currentTimeMillis();
-        System.out.println(Arrays.toString(AsteroidCollision.asteroidCollision(x)));
+        System.out.println(DecodeString.decodeString(word1));
         long fin = System.currentTimeMillis();
         System.out.println("Tiempo de Ejecucicon: " + (fin - inicio) + "ms");
 
---
+++ stack/DecodeString.java
@@ -0,0 +1,33 @@
+package stack;
+
+public class DecodeString {
+    static int  i = 0;
+    public static String decodeString(String s) {
+        i = 0;
+        return decode(s);
+    }
+    private static String decode(String s) {
+        StringBuilder res = new StringBuilder();
+        int num = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+                i++;
+            } else if (c == '[') {
+                i++;
+                String inner = decode(s);
+                for (int k = 0; k < num; k++) res.append(inner);
+                num = 0;
+            } else if (c == ']') {
+                i++;
+                return res.toString();
+            } else {
+                res.append(c);
+                i++;
+            }
+        }
+        return res.toString();
+    }
+
+}
